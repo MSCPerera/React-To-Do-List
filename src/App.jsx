0b91ce086f901bbf663aa64728bbdec31db91d09
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-04-03 11:54:42 +0530

    Update Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,50 @@ import React, {useState, useEffect} from "react";
       localStorage.setItem("todo-data", JSON.stringify([]))
     }
 
-  }, [])
+  }, []);
 
-  return <div>App</div>
+  const addNewTodo = () => {
+    const newTodo = {text: input, complete: false}
+    const updatedTodoList = [...data, newTodo]
+    setDate(updatedTodoList);
+    localStorage.setItem("todo-data", JSON.stringify(updatedTodoList))
+    setInput("");
+  }
+
+  return (
+    <div>
+      <input 
+        value={input} 
+        type="text" 
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <button
+       onClick={() => {
+        addNewTodo;
+        }}
+      >
+        Add new todo
+      </button>
+
+      <hr />
+
+      <div>
+        {data.length > 0 ? (
+          <div>
+            <ol>
+              {data.map((todo, index) => (
+                <p>{todo.text}</p>
+              ))}
+            </ol>
+          </div>
+        ) : (
+          <p>Add a new todo</p>
+        )}
+      </div>
+    </div>
+  );
  }
 
  export default App;
